76979461e37e4ee0b4b8742c81f61f7fe2ef775a
Misha Koryak <koryak@gmail.com>
2013-08-26 11:09:24 -0400

    update to latest version

--- src/jquery.dropdownLite.js
+++ src/jquery.dropdownLite.js
@@ -2,8 +2,6 @@
  * version 1.0 - 9/11/2012
  * 
  * @requires: jquery 1.7.x
- * 
- * 
  * Create a widget, that when clicked will cause a "body" element to appear beneath it. You can have multiple widgets share a single body, 
  * and you can add new widgets to share a body after plugin creation.
  * 
@@ -11,16 +9,19 @@
  * 
  * the body element gets the following useful data bound to it: ($body.data(....))
  * $body.data('ddl-for') = returns the jQuery object reference to the widget that was clicked, and for which the body is shown
- * $body.data('ddl').addWidget($new_widget) = this is the function you would use to add new widgets to share the body element
+ * $body.data('ddl').addWidget($newWidget) = this is the function you would use to add new widgets to share the body element
  * 
  * each widget gets the following map attached as:
  * var ddl = $widget.data('ddl');
  * ddl.getBody() = returns the body element jquery ref
  * ddl.showBody() = shows the body for the current widget as if the widget was clicked
  * ddl.hideBody() = hides the body
- * 
- *  Dual licensed under the MIT and GPL licenses
- *    
+ */ 
+ 
+ /**
+ * @preserve jQuery.dropdownLite 1.0.1
+ * Copyright (c) 2013 Misha Koryak - https://github.com/mkoryak/jquery.dropdownLite
+ * Licensed under Creative Commons Attribution-NonCommercial 3.0 Unported - http://creativecommons.org/licenses/by-sa/3.0/
  */
 (function ($) {
     
@@ -32,8 +33,8 @@ $.fn.dropdownLite = function(map) {
             return $("<div>Please define a body</div>")
         },
         positionOffset: "", //see http://jqueryui.com/demos/position/#option-offset
-        onShowBody: function($widget) {},
-        onHideBody: function() {}, //this happens when body is hidden
+        onShowBody: function($widget, $body) {},
+        onHideBody: function($body) {}, //this happens when body is hidden
         positionBodyAt: "left bottom", //postion the body here relative to the widget
         positionBodyMy: 'left top', //postion 'my'
         positionCollision: 'flip flip', //postion 'my'
@@ -44,8 +45,11 @@ $.fn.dropdownLite = function(map) {
         hideFn: function($body){ //override to hide in a fancy way
             $body.hide();
         }, 
-        positionOf: function($widget){ //we postion around this element, see the 'of' property in the position plugin. override if needed
+        positionOf: function($widget){ //we position around this element, see the 'of' property in the position plugin. override if needed
             return $widget;
+        },
+        positionFn: function($widget, $body, pos){ //override what happens when $body is positioned. you must set top/left in this function
+            $body.css({top: pos.top, left:pos.left});
         }
     }, map || {});
     
@@ -54,6 +58,9 @@ $.fn.dropdownLite = function(map) {
     }
 
     var $body = opts.body().hide();
+    if($body.length == 0){
+        return this;
+    }
     $body.css({'position':'absolute'}).addClass('ddl-body');
     var showingBodyFor = -1;
     var numberWidgets = this.length;
@@ -77,16 +84,19 @@ $.fn.dropdownLite = function(map) {
                 'my':opts.positionBodyMy,
                 'at': opts.positionBodyAt,
                 'offset': opts.positionOffset,
-                'collision': opts.positionCollision
+                'collision': opts.positionCollision,
+                'using': function(pos){
+                    opts.positionFn($this, $body, pos);
+                }
             });
             $body.data('ddl-for', $this);
         }
-        opts.onShowBody($this);
+        opts.onShowBody($this, $body);
     };
 
     var hideBody = function() {
         opts.hideFn($body);
-        opts.onHideBody();
+        opts.onHideBody($body);
     };
 
     $(document).bind("click.ddl", function(e) {
@@ -115,7 +125,7 @@ $.fn.dropdownLite = function(map) {
                 showBody($this);
             }
             $(document).trigger("ddl-hide-body-global", [$body.data('ddl-global-id')]);
-            e.stopPropagation();
+         //TODO: why is this here?   e.stopPropagation();;
         });
 
         $this.data("ddl", {
@@ -140,14 +150,6 @@ $.fn.dropdownLite = function(map) {
     });
     return this;
 };
-
-/**
- * 
- * hover plugin: show dropdown on hover of 'delayMs'
- * 
- * @param map
- * @return {*}
- */
 $.fn.hoverDropdownLite = function(map) {
     var opts = $.extend({}, {
         delayMs: 10
@@ -186,5 +188,33 @@ $.fn.hoverDropdownLite = function(map) {
         });
     });
     return this;
-}
+};
+$.fn.collisionAwareDropdown = function(map){
+    var opts = $.extend({}, {
+        positionBodyAt: "left bottom",
+        positionBodyMy: 'left top', 
+        positionCollision: 'flip flip' 
+    }, map || {});
+    if(opts.positionBodyAt == 'left bottom' && opts.positionBodyMy == 'left top' && opts.positionCollision == 'flip flip'){
+        opts = $.extend({}, {
+            positionFn: function($widget, $body, pos){ //override what happens when $body is positioned. you must set top/left in this function
+                var offset = $widget.offset();
+                offset.left = ~~offset.left;
+                offset.top = ~~offset.top;
+                if(offset.left > pos.left){ //the left of the dropdown should be under the left of the link, if not, there was a collision with the edge of the screen so we need to reposition the little arrow thingy by adding a class
+                    $body.addClass('collision-x');
+                } else {
+                    $body.removeClass('collision-x');
+                }
+                if(offset.top > pos.top){ //the left of the dropdown should be under the left of the link, if not, there was a collision with the edge of the screen so we need to reposition the little arrow thingy by adding a class
+                    $body.addClass('collision-y');
+                } else {
+                    $body.removeClass('collision-y');
+                }
+                $body.css({top: pos.top, left:pos.left});
+            }
+        }, opts);
+    } 
+    return this.dropdownLite(opts);
+};
 }(jQuery));
